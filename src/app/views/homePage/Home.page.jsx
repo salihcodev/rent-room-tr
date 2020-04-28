212c69165618d87457b06f35e34fa7ad80cd23cf
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-04-29 01:49:29 +0200

    Create about section component (#5)
    - Setup the basics of the comonent.
    - Add the component to homePage.

--- src/app/views/homePage/Home.page.jsx
+++ src/app/views/homePage/Home.page.jsx
@@ -3,24 +3,33 @@ import React from "react";
 
 // COMPONENTS IMPORTS::==>
 import Header from "../../components/header/Header.component";
+import CompanyPhilosophy from "../../components/philosophy/CompanyPhilosophy.component";
 
 // UTILITIES IMPORTS::==>
 
 // COMPONENT IMPORTS::==>
+import "./Home.page.style.scss";
 import HeaderImg from "../../assets/imgs/defaultBcg.jpeg";
 const HomePage = () => {
-  const title = "Let us give you a nice bed!";
+  // home required data:
+  // header:
+  const headerTitle = "Let us give you a nice bed!";
+
+  // about section:
+  const aboutTitle = "hello from about company";
+  const aboutText =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque repellendus sapiente qui modi perferendis dicta, earum accusantium quis aliquid! Voluptatibus aliquid ducimus dolorum nesciunt repudiandae quibusdam eius debitis quos explicabo!";
+
   return (
     <>
       <Header
         img={HeaderImg}
-        title={title}
+        title={headerTitle}
         btn1="return to home"
         btn2="browse rooms"
-      >
-        <p>this is Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, veniam. children</p>
-      </Header>
-      <p>Home</p>
+      ></Header>
+
+      <CompanyPhilosophy aboutTitle={aboutTitle} aboutContext={aboutText} />
     </>
   );
 };
