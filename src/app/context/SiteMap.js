4013269d11893662020b45d61825487cb8b76fe4
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-04-28 04:07:26 +0200

    End the first version of footer (#3)
    - Allmost all necessary filles had been setup
    - Ther is new file add to context folder.

---
+++ src/app/context/SiteMap.js
@@ -0,0 +1,12 @@
+const SiteMap = [
+  { id: 0, text: "services", link: "/" },
+  { id: 1, text: "services", link: "/" },
+  { id: 2, text: "services", link: "/" },
+  { id: 3, text: "services", link: "/" },
+  { id: 4, text: "services", link: "/" },
+  { id: 5, text: "services", link: "/" },
+  { id: 6, text: "services", link: "/" },
+  { id: 7, text: "services", link: "/" },
+];
+
+export default SiteMap;
