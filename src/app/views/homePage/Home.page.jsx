dff4bbc01ab8a06dd5af79e7678a5cbf7ee7ee73
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-04-29 01:18:47 +0200

    Add the basics elements to header component (#4)
    - create header component
    - test logic between header and homePage to make sure every thing is ok.

--- src/app/views/homePage/Home.page.jsx
+++ src/app/views/homePage/Home.page.jsx
@@ -2,12 +2,27 @@
 import React from "react";
 
 // COMPONENTS IMPORTS::==>
+import Header from "../../components/header/Header.component";
 
 // UTILITIES IMPORTS::==>
 
 // COMPONENT IMPORTS::==>
+import HeaderImg from "../../assets/imgs/defaultBcg.jpeg";
 const HomePage = () => {
-  return <div>Home</div>;
+  const title = "Let us give you a nice bed!";
+  return (
+    <>
+      <Header
+        img={HeaderImg}
+        title={title}
+        btn1="return to home"
+        btn2="browse rooms"
+      >
+        <p>this is Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, veniam. children</p>
+      </Header>
+      <p>Home</p>
+    </>
+  );
 };
 
 export default HomePage;
