4392545b9f96e8a2cd8e089fd5f4438b2f9616cc
Ahmed Salih <ahmedsalihdev@gmail.com>
2020-04-29 01:17:30 +0200

    Add the basics elements to header component - create header component - test logic between header and homePage to make sure every thing is ok.

---
+++ src/app/components/header/Header.component.jsx
@@ -0,0 +1,34 @@
+// BASIC IMPORTS::==>
+import React from "react";
+
+// COMPONENTS IMPORTS::==>
+
+// UTILITIES IMPORTS::==>
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+// COMPONENT IMPORTS::==>
+import "./Header.component.style.scss";
+const Header = ({ img, title, btn1, btn2, children }) => {
+  return (
+    <MainHeader img={img} className="mainHeader">
+      <h2 className="headerHeading">{title}</h2>
+      <div className="headerBody">
+        <div className="headerContentWrapper">{children}</div>
+      </div>
+      <div className="buttons">
+        <div className="headerBtnsWrapper">
+          <button className="btn btn-warning">{btn1}</button>
+          <button className="btn btn-info">{btn2}</button>
+        </div>
+      </div>
+    </MainHeader>
+  );
+};
+
+export default Header;
+// i used styled component just for get benefit of using props inside <style>.
+const MainHeader = styled.header`
+  background: linear-gradient(90deg, #18b08d3d, #19467fe0),
+    url(${(props) => props.img}) center/cover;
+`;
