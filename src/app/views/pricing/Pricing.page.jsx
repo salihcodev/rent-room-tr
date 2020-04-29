545a105ea474cd2606e58114c88ba789e88df309
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-04-30 01:05:53 +0200

    Create room component viewer (#8)
    - Add room template to generate room component into multibale places.
    - Destructe necessary data to context from data.js file.
    - Add the basics of style to prices page.

--- src/app/views/pricing/Pricing.page.jsx
+++ src/app/views/pricing/Pricing.page.jsx
@@ -1,13 +1,52 @@
 // BASIC IMPORTS::==>
 import React from "react";
-
+import { AppContext } from "../../context";
 // COMPONENTS IMPORTS::==>
+import RoomTemplate from "../../components/room/RoomTemplate.component";
 
 // UTILITIES IMPORTS::==>
 
 // COMPONENT IMPORTS::==>
-const PricingPage = () => {
-  return <div>PricingPage</div>;
-};
+import "./Pricing.page.style.scss";
+class PricingPage extends React.Component {
+  static contextType = AppContext;
+  render() {
+    const value = this.context;
+    const { featuredRooms, allRooms } = value;
+    return (
+      <>
+        <section className="featuredRoomsWrapper roomWrapper">
+          <div className="container">
+            <h2 className="heading">featured rooms</h2>
+            <div className="row">
+              {featuredRooms.map((room) => {
+                return (
+                  <div className="col-md-4">
+                    <RoomTemplate room={room} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+
+        <section className="allRoomsWrapper roomWrapper">
+          <div className="container">
+            <h2 className="heading">all rooms</h2>
+            <div className="row">
+              {allRooms.map((room) => {
+                return (
+                  <div className="col-md-3">
+                    <RoomTemplate room={room} />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
+}
 
 export default PricingPage;
