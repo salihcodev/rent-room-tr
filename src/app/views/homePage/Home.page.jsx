e62f59bd2a6943c7ba8490fdcd3bebc4630175ce
Ahmed Salih <45488156+salihcodev@users.noreply.github.com>
2020-04-29 05:23:03 +0200

    Add another component to view site prices pans (#6)
    - Add the basic layout.

--- src/app/views/homePage/Home.page.jsx
+++ src/app/views/homePage/Home.page.jsx
@@ -4,6 +4,7 @@ import React from "react";
 // COMPONENTS IMPORTS::==>
 import Header from "../../components/header/Header.component";
 import CompanyPhilosophy from "../../components/philosophy/CompanyPhilosophy.component";
+import PlansWrapper from "../../components/plans/PlansWrapper.component";
 
 // UTILITIES IMPORTS::==>
 
@@ -13,23 +14,78 @@ import HeaderImg from "../../assets/imgs/defaultBcg.jpeg";
 const HomePage = () => {
   // home required data:
   // header:
-  const headerTitle = "Let us give you a nice bed!";
+  const HeaderTitle = "Let us give you a nice bed!";
 
   // about section:
-  const aboutTitle = "hello from about company";
-  const aboutText =
+  const AboutTitle = "hello from about company";
+  const AboutText =
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque repellendus sapiente qui modi perferendis dicta, earum accusantium quis aliquid! Voluptatibus aliquid ducimus dolorum nesciunt repudiandae quibusdam eius debitis quos explicabo!";
+  const plansData = [
+    {
+      id: 0,
+      planeName: "ultimate",
+      featuresList: [
+        "featureIndex: bar",
+        "featureOne: bar",
+        "featureTwo: bar",
+        "featureThree: bar",
+        "featureFour: bar",
+        "featureFive: bar",
+        "featureSix: bar",
+        "featureSeven: bar",
+      ],
+      ship: "ship Now",
+      planeCost: "$ 212",
+    },
+    {
+      id: 1,
+      planeName: "educational",
+      featuresList: [
+        "featureIndex: bar",
+        "featureOne: bar",
+        "featureTwo: bar",
+        "featureThree: bar",
+        "featureFour: bar",
+        "featureFive: bar",
+        "featureSix: bar",
+        "featureSeven: bar",
+      ],
+      ship: "ship Now",
+      planeCost: "$ 150",
+    },
+    {
+      id: 2,
+      planeName: "basic",
+      featuresList: [
+        "featureIndex: bar",
+        "featureOne: bar",
+        "featureTwo: bar",
+        "featureThree: bar",
+        "featureFour: bar",
+        "featureFive: bar",
+        "featureSix: bar",
+        "featureSeven: bar",
+      ],
+      ship: "ship Now",
+      planeCost: "$ 75",
+    },
+  ];
 
   return (
     <>
+      {/* HEADER */}
       <Header
         img={HeaderImg}
-        title={headerTitle}
+        title={HeaderTitle}
         btn1="return to home"
         btn2="browse rooms"
       ></Header>
 
-      <CompanyPhilosophy aboutTitle={aboutTitle} aboutContext={aboutText} />
+      {/* ABOUT THE COMPANY */}
+      <CompanyPhilosophy aboutTitle={AboutTitle} aboutContext={AboutText} />
+
+      {/* PLANS */}
+      <PlansWrapper plansData={plansData} />
     </>
   );
 };
