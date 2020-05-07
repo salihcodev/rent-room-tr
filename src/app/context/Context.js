5984a332a20116ba5f616352717181b6112870ba
Ahmed Salih <ahmedsalihdev@gmail.com>
2020-05-07 02:30:05 +0200

    Fix duplicated code bug!

--- src/app/context/Context.js
+++ src/app/context/Context.js
@@ -2,6 +2,7 @@
 import React from "react";
 
 // UTILITIES IMPORTS::==>
+import AllData from "./data";
 import BrowsingLinks from "./BrowsingLinks";
 import SocialMediaLinks from "./SocialMediaLinks";
 import TestimonialsData from "./TestimonialsData";
@@ -24,6 +25,62 @@ class AppProvider extends React.Component {
     Support: Support,
     Company: Company,
     companyExperience: companyExperience,
+    allRooms: [],
+    featuredRooms: [],
+    singleRoomInfo: {},
+  };
+
+  componentDidMount() {
+    this.fetchData(AllData); //allData
+  }
+
+  fetchData = (allData) => {
+    let allRooms = allData.map((room) => {
+      const { id } = room.sys;
+      const img = room.fields.images[0].fields.file.url;
+      const images = room.fields.images.map((img) => {
+        return img.fields.file.url;
+      });
+      return { id, ...room.fields, img, images };
+    });
+
+    let featuredRooms = allRooms.filter((fRoom) => {
+      return fRoom.featured === true;
+    });
+
+    this.setState({
+      allRooms,
+      featuredRooms,
+      singleRoomInfo: this.getLocalSingleRoom(),
+    });
+  };
+
+  getLocalSingleRoom = () => {
+    let getSingleRoom = localStorage.getItem("singleRoom");
+
+    return getSingleRoom ? JSON.parse(getSingleRoom) : {};
+  };
+
+  setLocalSingleRoom = (id) => {
+    let sRoom = this.state.allRooms.find((sRoom) => sRoom.id === id);
+    // localStorage here
+    localStorage.setItem("singleRoom", JSON.stringify(sRoom));
+    window.scrollTo({
+      top: 188,
+      left: 0,
+      behavior: "smooth",
+    });
+
+    this.setState({
+      singleRoomInfo: { ...sRoom },
+    });
+  };
+
+  // toggle alt navLinks:
+  navToggler = () => {
+    this.setState({
+      isSmall: !this.state.isSmall,
+    });
   };
 
   render() {
